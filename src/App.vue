ac73b3f2f71a9dc2668f639729fa845ca24edd6a
Young-TW <young20050727@gmail.com>
2021-11-25 13:45:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <componentA />
+</template>
+
+<script setup>
+  import componentA from './components/componentA.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/componentA.vue
@@ -0,0 +1,40 @@
+<template>
+  <button @click="changeView(1)">1</button>
+  <button @click="changeView(2)">2</button>
+  <button @click="changeView(123)">123</button>
+  <div class="c1" v-if="view === 1">
+    <p>hello world</p>
+    {{data}}
+  </div>
+  <div v-else>
+    <p>
+      nothing
+    </p>
+  </div>
+  <div class="c2" v-if="view === 2">
+    <input type="text" v-model="text"/>
+    <p>
+      {{text}}
+    </p>
+  </div>
+  <div v-else>
+    <p>
+      nothing
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "@vue/reactivity"
+
+const data = "hello young"
+const text = ref("123")
+const view = ref(1)
+const changeView = (index) => {
+  view.value = index
+}
+</script>
+
+<style>
+
+</style>
